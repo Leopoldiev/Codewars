2d90ec58513a2e97dfa6ac97325d87f764f92a54
Leopoldiev <leoflamefire777@gmail.com>
2025-03-26 14:30:33 +0300

    Kata completed: Grasshopper - Terminal

---
+++ 8kyu/Grasshopper-Terminal.js
@@ -0,0 +1,9 @@
+// ❗ Description:
+// Create a combat function that takes the player's current health and the amount of damage received, and returns the player's new health. Health can't be less than 0.
+
+// ✅ SOLUTION:
+function combat(health, damage) {
+   let newHealth = health - damage;
+   if (newHealth < 0) { return 0 }
+   else return newHealth;
+}
