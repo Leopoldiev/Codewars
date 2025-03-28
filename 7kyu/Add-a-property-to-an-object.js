aafad5793fca94ba3283774ad55977b59238f818
Leopoldiev <leoflamefire777@gmail.com>
2025-03-28 19:41:13 +0300

    Kata completed: Add a property to an object

--- 7kyu/Add-a-property-to-an-object.js
+++ 7kyu/Add-a-property-to-an-object.js
@@ -1,5 +1,5 @@
-❗ Description:
-Write a function that adds a named property to an object.It must be possible to set the property to a new value.If the property already exists on the object, and error should be thrown.
+// ❗ Description:
+// Write a function that adds a named property to an object.It must be possible to set the property to a new value.If the property already exists on the object, and error should be thrown.
 
 // ✅ SOLUTION:
 
